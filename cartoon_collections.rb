6c06d43d2d82a076cbb4c5b0e7e88d94df9beb01
Richard Huelves <rhuelves@gmail.com>
2019-08-22 14:56:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,8 +16,8 @@ end
 
 planeteer_calls = %w[earth wind fire water heart]
 
-def summon_captain_planet
-  
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |call| call.capitalize + '!' }
 end
 
 def long_planeteer_calls# code an argument here
