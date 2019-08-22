e8e35be54ff9695d906d8003ecae1bba7e0a9487
Richard Huelves <rhuelves@gmail.com>
2019-08-22 15:13:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,6 +24,7 @@ def long_planeteer_calls
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
+potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
 def find_the_cheese# code an argument here
   # the array below is here to help
