50642fa378cb14203f7bc36a2f3c314145471bb5
Richard Huelves <rhuelves@gmail.com>
2019-08-22 15:25:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -25,8 +25,18 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.any? { |call| call.length > 4 }
 end
 
+# potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
+
+# def find_the_cheese
+#   cheese_types = ["cheddar", "gouda", "camembert"]
+# end
+
 potentially_cheesy_items = %w[umbrella spinach cheddar helicopter]
 
-def find_the_cheese
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(potentially_cheesy_items)
+  cheeses = %w[gouda cheddar camembert]
+
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
